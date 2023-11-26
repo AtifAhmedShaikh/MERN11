dd8a92101b7494bf5ac59e75a1f30e0642eded07
AtifAhmedShaikh <atifahmad2219@gmail.com>
2023-11-26 08:09:43 -0800

    remove connection string

--- task10/backend/src/connectDB.js
+++ task10/backend/src/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     try{
-      const connect = await mongoose.connect("mongodb+srv://atifahmad2219:ecommerceapp@cluster0.malln0y.mongodb.net/store?retryWrites=true&w=majority");//don't show this Ok
+      const connect = await mongoose.connect("my-connection-string-of-campass");//don't show this Ok
       console.log("Database connect successfully",connect.connection.host);
     }catch(error){
       console.log("Database connection Error !",error);
