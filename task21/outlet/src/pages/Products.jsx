a86347494d8feab46770292b29969bf612c804cd
AtifAhmedShaikh <atifahmad2219@gmail.com>
2023-12-15 02:47:34 -0800

    cart manage with redux

--- task21/outlet/src/pages/Products.jsx
+++ task21/outlet/src/pages/Products.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import data from "../data/products.json";
 export default function Products() {
-  return <div>Products</div>;
+  return <div>
+    {
+      data.map(item=>{
+        return <div key={item.productId}>
+          <img src="" className="w-10 h-10"  alt="" />
+          <p>{item.description}</p>
+          <p>{item.category}</p>
+          <p>{item.name}</p>
+        </div>
+      })
+    }
+    {JSON.stringify(data)}
+  </div>;
 }
